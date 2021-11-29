e00a14a3f55736f2eda576c5778c27138a994d03
braulio flores <bfloresl1400@alumno.ipn.mx>
2021-11-29 12:23:54 -0600

    primer commit con conextion a bd en db4free

---
+++ Login.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="FinalProject.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login del sistema</h1>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using MySql.Data.MySqlClient;
+
+namespace FinalProject
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cCone = "Server=db4free.net;UserID=valdepenia;Database=finalprojectvald;Password=valdepeniaroot"; 
+            //string cCone = "Server=localhost;UserID=root;Database=segobajax;Password=root";
+            using (var con = new MySqlConnection(cCone))
+            {
+                con.Open();
+                using (var cmd = new MySqlCommand("SELECT * FROM users", con))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        DropDownList1.DataSource = reader;
+                        DropDownList1.DataValueField = "nombre";
+                        DropDownList1.DataTextField = "correo";
+                        DropDownList1.DataBind();
+                    }
+                }
+            }
+        }
+    }
+}
